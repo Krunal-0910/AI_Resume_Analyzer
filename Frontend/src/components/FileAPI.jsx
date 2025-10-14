bc000ed4cfa67aa7982f702913b0062c1a6c8f8f
Krunal-0910 <khsenjaliya2001@gmail.com>
2025-10-14 17:37:18 -0500

    Uncommented Test Output

--- Frontend/src/components/FileAPI.jsx
+++ Frontend/src/components/FileAPI.jsx
@@ -7,16 +7,7 @@ const FileApi = async ( fileParse,setResults,setAnalysis_complete,setShowAnalyzi
                 if (!request.ok){
                     throw new Error("Server not responding")
                 }
-                const data = await request.json()
-            // const data=
-            // {'summary': 'A Computer Science student with a strong GPA and hands-on experience developing a social media web application using React, Node.js, and MySQL. Eager to apply technical skills and contribute to innovative projects, ideally in a software engineering role.', 
-            //     'strengths': ['Full-stack development', 'Database design and management', 'Problem-solving', 'Rapid learning', 'Teamwork'],
-            //      'skills': ['React', 'Node.js', 'RESTful API', 'MySQL', 'JavaScript', 'Java', 'UI Design', 'Database Management'], 
-            //      'role_fit': 'Software Engineer Intern', 'experience_level': 'Entry', 'recommendations': ['Expand on project details in the resume to showcase specific accomplishments and technologies used.', "Quantify achievements whenever possible (e.g., 'Improved database query performance by X%').", 'Include a link to the personal project on GitHub or a live demo, if available.', 'Tailor the resume to specific job descriptions, highlighting the most relevant skills and experiences.'], 
-            //     'ats_score': 75}
-
-
-                
+                const data = await request.json()              
                 setResults(data)
                 setAnalysis_complete(true)
                 setShowAnalyzing(false)
